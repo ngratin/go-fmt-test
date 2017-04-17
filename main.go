a00f1c7d5b14078cdf4737ee64820158aaa6b85d
Masafumi Noguchi <ngratin@gmail.com>
2017-04-17 17:09:12 +0900

    test

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	latest "github.com/tcnksm/go-latest"
+)
 
 func main() {
+	githubTag := &latest.GithubTag{
+		Owner:      "ngratin",
+		Repository: "go-fmt-test",
+	}
+
+	res, _ := latest.Check(githubTag, "0.1.0")
+	if res.Outdated {
+		fmt.Println(res.Current)
+		fmt.Println("outdate")
+	}
+
 	fmt.Println("hello")
 }
